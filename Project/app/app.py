ecfee326d123d764d98a9260e06f334aa8f89e51
GenerationK <generationk08@gmail.com>
2021-01-02 13:04:17 +0530

    sessions completely implemented

--- Project/app/app.py
+++ Project/app/app.py
@@ -104,130 +104,174 @@ def teacherprofile():
 @app.route("/admin/addbook",methods=["GET","POST"])
 def addbook():
     status = None
-    if request.method == "POST":
-        req = request.form
-        status = book_val(req)
-        if(status != "Success"):
-            return redirect("/admin/addbook")
-        else:
-            return redirect("/admin")
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            req = request.form
+            status = book_val(req)
+            if(status != "Success"):
+                return redirect("/admin/addbook")
+            else:
+                return redirect("/admin")
+    else:
+        return redirect("/adminlogin")
     return render_template("addbook.html")
 
 @app.route("/admin/addstudent",methods=["GET","POST"])
 def addstudent():
     status = None
-    if request.method == "POST":
-        req = request.form
-        status = stu_val(req)
-        if(status != "Success"):
-            return redirect("/admin/addstudent")
-        else:
-            return redirect("/admin")
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            req = request.form
+            status = stu_val(req)
+            if(status != "Success"):
+                return redirect("/admin/addstudent")
+            else:
+                return redirect("/admin")
+    else:
+        return redirect("/adminlogin")
     return render_template("addstudent.html")
 
 @app.route("/admin/removebook",methods=["GET","POST"])
 def removebook():
-    if request.method == "POST":
-        req = request.form
-        status = book_del(req)
-        if(status == "Success"):
-            return redirect("/admin")
-        else:
-            return redirect("/admin/removebook")
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            req = request.form
+            status = book_del(req)
+            if(status == "Success"):
+                return redirect("/admin")
+            else:
+                return redirect("/admin/removebook")
+    else:
+        return redirect("/adminlogin")
     return render_template("removebook.html")
 
 @app.route("/admin/removestudent",methods=["GET","POST"])
 def removestudent():
-    if request.method == "POST":
-        req = request.form
-        status = stu_del(req)
-        if(status == "Success"):
-            return redirect("/admin")
-        else:
-            return redirect("/admin/removestudent")
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            req = request.form
+            status = stu_del(req)
+            if(status == "Success"):
+                return redirect("/admin")
+            else:
+                return redirect("/admin/removestudent")
+    else:
+        return redirect("/adminlogin")
     return render_template("removestudent.html")
 
 @app.route("/admin/viewbooks",methods=["GET","POST"])
 def viewbooks():
-    data = list()
-    data = book_display()
-    if not data:
-        msg = "No Books"
+    if "user" in session:
+        user = session["user"]
+        data = list()
+        data = book_display()
+        if not data:
+            msg = "No Books"
+        else:
+            msg = "The Books data has been written on the Book.csv file in the Downloads folder"
+            book_write(data)
+        flash(msg)
     else:
-        msg = "The Books data has been written on the Book.csv file in the Downloads folder"
-    flash(msg)
-    book_write(data)
+        return redirect("/adminlogin")
     return render_template("viewbooks.html")
 
 @app.route("/admin/viewstudents",methods=["GET","POST"])
 def viewstudents():
-    data = list()
-    data = stu_display()
-    if not data:
-        msg = "No Student Users"
+    if "user" in session:
+        user = session["user"]
+        data = list()
+        data = stu_display()
+        if not data:
+            msg = "No Student Users"
+        else:
+            stu_write(data)
+            msg = "The Student Users data has been written on the Student_Users.csv file in the Downloads folder"
+        flash(msg)
     else:
-        stu_write(data)
-        msg = "The Student Users data has been written on the Student_Users.csv file in the Downloads folder"
-    flash(msg)
+        return redirect("/adminlogin")
     return render_template("viewstudents.html")
 
 @app.route("/admin/bookhistory")
 def bookhistory():
-    data = list()
-    data = book_history()
-    if not data:
-        msg = "No Book History"
+    if "user" in session:
+        user = session["user"]
+        data = list()
+        data = book_history()
+        if not data:
+            msg = "No Book History"
+        else:
+            msg = "The Books History data has been written on the Books_History.csv file in the Downloads folder"
+            book_history_csv(data)
+        flash(msg)
     else:
-        msg = "The Books History data has been written on the Books_History.csv file in the Downloads folder"
-        book_history_csv(data)
-    flash(msg)
+        return redirect("/adminlogin")
     return render_template("bookhistory.html")
 
 @app.route("/admin/studenthistory",methods=["GET","POST"])
 def studenthistory():
-    data = list()
-    data = stu_history()
-    if not data:
-        msg = "No Student History"
+    if "user" in session:
+        user = session["user"]
+        data = list()
+        data = stu_history()
+        if not data:
+            msg = "No Student History"
+        else:
+            msg = "The Student History data has been written on the Students_History.csv file in the Downloads folder"
+            stu_history_csv(data)
+        flash(msg)
     else:
-        msg = "The Student History data has been written on the Students_History.csv file in the Downloads folder"
-        stu_history_csv(data)
-    flash(msg)
+        return redirect("/adminlogin")
     return render_template("studenthistory.html",data=data)
 
 @app.route("/admin/addteacher",methods=["GET","POST"])
 def addteacher():
     status = None
-    if request.method == "POST":
-        req = request.form
-        status = teach_val(req)
-        if(status != "Success"):
-            return redirect("/admin/addteacher")
-        else:
-            return redirect("/admin")
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            req = request.form
+            status = teach_val(req)
+            if(status != "Success"):
+                return redirect("/admin/addteacher")
+            else:
+                return redirect("/admin")
+    else:
+        return redirect("/adminlogin")
     return render_template("addteacher.html")
 
 @app.route("/admin/removeteacher",methods=["GET","POST"])
 def removeteacher():
-    if request.method == "POST":
-        req = request.form
-        status = teach_del(req)
-        if(status == "Success"):
-            return redirect("/admin")
-        else:
-            return redirect("/admin/removeteacher")
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            req = request.form
+            status = teach_del(req)
+            if(status == "Success"):
+                return redirect("/admin")
+            else:
+                return redirect("/admin/removeteacher")
+    else:
+        return redirect("/adminlogin")
     return render_template("removeteacher.html")
 
 @app.route("/admin/viewteachers",methods=["GET","POST"])
 def viewteachers():
-    data = list()
-    data = teach_display()
-    if not data:
-        msg = "No Teacher Users"
+    if "user" in session:
+        user = session["user"]
+        data = list()
+        data = teach_display()
+        if not data:
+            msg = "No Teacher Users"
+        else:
+            msg = "The Teacher Users data has been written on the Teacher_Users.csv file in the Downloads folder"
+            teach_write(data)
+        flash(msg)
     else:
-        msg = "The Teacher Users data has been written on the Teacher_Users.csv file in the Downloads folder"
-        teach_write(data)
-    flash(msg)
+        return redirect("/adminlogin")
     return render_template("viewteachers.html")
 
 @app.route("/admin/teacherhistory")
@@ -244,35 +288,47 @@ def teacherhistory():
 
 @app.route("/admin/bookreturn",methods=["GET","POST"])
 def bookreturn():
-    if request.method=="POST":
-        req = request.form
-        status = returnbook(req)
-        if status == "Success":
-            return redirect("/admin")
-        else:
-            return redirect("/admin/bookreturn")
+    if "user" in session:
+        user = session["user"]
+        if request.method=="POST":
+            req = request.form
+            status = returnbook(req)
+            if status == "Success":
+                return redirect("/admin")
+            else:
+                return redirect("/admin/bookreturn")
+    else:
+        return redirect("/adminlogin")
     return render_template("bookreturn.html")
 
 @app.route("/admin/studentborrow",methods=["GET","POST"])
 def studentborrow():
-    if request.method=="POST":
-        req = request.form
-        status = admin_stu_borrow(req)
-        if status == "Success":
-            return redirect("/admin")
-        else:
-            return redirect("/admin/studentborrow")
+    if "user" in session:
+        user = session["user"]
+        if request.method=="POST":
+            req = request.form
+            status = admin_stu_borrow(req)
+            if status == "Success":
+                return redirect("/admin")
+            else:
+                return redirect("/admin/studentborrow")
+    else:
+        return redirect("/adminlogin")
     return render_template("studentborrow.html")
 
 @app.route("/admin/teacherborrow",methods=["GET","POST"])
 def teacherborrow():
-    if request.method=="POST":
-        req = request.form
-        status = admin_teach_borrow(req)
-        if status == "Success":
-            return redirect("/admin")
-        else:
-            return redirect("/admin/teacherborrow")
+    if "user" in session:
+        user = session["user"]
+        if request.method=="POST":
+            req = request.form
+            status = admin_teach_borrow(req)
+            if status == "Success":
+                return redirect("/admin")
+            else:
+                return redirect("/admin/teacherborrow")
+    else:
+        return redirect("/adminlogin")
     return render_template("teacherborrow.html")
 
 @app.route("/studentprofile/studetails")
@@ -302,25 +358,33 @@ def teachdetails():
 @app.route("/studentprofile/stuupdate",methods=["GET","POST"])
 def stuupdate():
     status = None
-    if request.method == "POST":
-        req = request.form
-        status = update(req,session["user"],"s")
-        if status == "Success":
-            return redirect("/studentprofile")
-        else:
-            return redirect("/studentprofile/stuupdate")
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            req = request.form
+            status = update(req,session["user"],"s")
+            if status == "Success":
+                return redirect("/studentprofile")
+            else:
+                return redirect("/studentprofile/stuupdate")
+    else:
+        return redirect("/student_login")
     return render_template("stuupdate.html")
 
 @app.route("/teacherprofile/teachupdate",methods=["GET","POST"])
 def teachupdate():
     status = None
-    if request.method == "POST":
-        req = request.form
-        status = update(req,session["user"],"t")
-        if status == "Success":
-            return redirect("/teacherprofile")
-        else:
-            return redirect("/teacherprofile/teachupdate")
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            req = request.form
+            status = update(req,session["user"],"t")
+            if status == "Success":
+                return redirect("/teacherprofile")
+            else:
+                return redirect("/teacherprofile/teachupdate")
+    else:
+        return redirect("/teacher_login")
     return render_template("teachupdate.html")
 
 @app.route("/studentprofile/stuavailbooks")
